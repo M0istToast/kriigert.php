2825930231a06af5badc58099db4172eee18c108
M0istToast <lagzdadavis383@gmail.com>
2026-01-21 15:11:13 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    "database" => [
+        "host" => "localhost",
+        "port" => 3306,
+        "user" => "root",
+        "password" => "",
+        "dbname" => "blog",
+        "charset" => "utf8mb4"
+    ],
+    "email" => [
+
+    ],
+    "payments" => [
+
+    ]
+];
---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+
+FUNCTION dd($value) {
+    echo"<pre>";
+    var_dump ($value);
+    echo"</pre>";
+    die();
+    }
